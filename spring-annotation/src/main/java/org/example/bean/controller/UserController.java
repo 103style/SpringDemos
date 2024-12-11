24e8c3a39823aedf9856d245710ccae4a3345d32
103style <wtimexiaoke@gmail.com>
2024-12-11 20:45:20 +0800

    Autowired set方法 构造函数 构造函数行参注入 示例

--- spring-annotation/src/main/java/org/example/bean/controller/UserController.java
+++ spring-annotation/src/main/java/org/example/bean/controller/UserController.java
@@ -9,8 +9,30 @@ import org.springframework.stereotype.Controller;
 public class UserController {
 
     // 第一种方式 属性注入
-    @Autowired
+//    @Autowired
+//    private IUserService service;
+
+
+    // 第二种方式 set方法注入
+//    private IUserService service;
+//    @Autowired
+//    public void setService(IUserService service) {
+//        this.service = service;
+//    }
+
+    // 第三种方式构造函数注入
+//    private IUserService service;
+//    @Autowired
+//    public UserController(IUserService service) {
+//        this.service = service;
+//    }
+
+
+    // 第四种方式 行参注入
     private IUserService service;
+    public UserController(@Autowired IUserService service) {
+        this.service = service;
+    }
 
     public void addUser() {
         System.out.println("addUser, service:" + service);
--- spring-annotation/src/main/java/org/example/bean/service/UserServiceImpl.java
+++ spring-annotation/src/main/java/org/example/bean/service/UserServiceImpl.java
@@ -9,8 +9,30 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements IUserService {
 
     // 第一种方式 属性注入
-    @Autowired
-    private IUserDao userDao;
+//    @Autowired
+//    private IUserDao userDao;
+
+    // 第二种方式 方法注入
+//    private IUserDao userDao;
+//
+//    @Autowired
+//    public void setUserDao(IUserDao userDao) {
+//        this.userDao = userDao;
+//    }
+
+
+//    // 第三种方式 构造函数注入
+//    private IUserDao userDao;
+//    @Autowired
+//    public UserServiceImpl(IUserDao userDao) {
+//        this.userDao = userDao;
+//    }
+
+    // 第四种方式 行参注入
+    private final IUserDao userDao;
+    public UserServiceImpl(@Autowired IUserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public void addUserService() {
