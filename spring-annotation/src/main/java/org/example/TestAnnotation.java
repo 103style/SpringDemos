b56b3f614ccc9d7b60cabed3f478f19a330ccc14
kempluo <kempluo@tencent.com>
2024-12-11 17:47:14 +0800

    component等注解测试

--- spring-annotation/src/main/java/org/example/TestAnnotation.java
+++ spring-annotation/src/main/java/org/example/TestAnnotation.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.example.bean.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestAnnotation {
+
+    @Test
+    public void test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+    }
 }
--- spring-annotation/src/main/java/org/example/bean/User.java
+++ spring-annotation/src/main/java/org/example/bean/User.java
@@ -2,6 +2,10 @@ package org.example.bean;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component(value = "user") // 等价于 <bean id="user" class="...."/>
+//@Repository(value = "user")
+//@Service(value = "user")
+//@Controller(value = "user")
+// 四个注解都可以，主要是不同的注解用于标识处理不同的功能类
 public class User {
 }
