7ff69f5fe2af25fad72654396f8f330059dc665b
103style <wtimexiaoke@gmail.com>
2024-12-16 19:56:06 +0800

    带参数的增删改查示例

--- MyBatisDemo/mybatis-02-params/src/main/java/com/style103/mybatis/TestMyBatisWithParams.java
+++ MyBatisDemo/mybatis-02-params/src/main/java/com/style103/mybatis/TestMyBatisWithParams.java
@@ -6,11 +6,14 @@ import com.style103.mybatis.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 /*
  * created by 103style on 2024/12/16 17:05
  * github:https://github.com/103style
  */
-public class  TestMyBatisWithParams {
+public class TestMyBatisWithParams {
 
     @Test
     public void testGetUserById() {
@@ -20,4 +23,81 @@ public class  TestMyBatisWithParams {
         System.out.println(user);
         sqlSession.close();
     }
+
+    @Test
+    public void testGetUserByName() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserByName("103style");
+        System.out.println(user);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testGetAllUser() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.getAllUser();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void testUpdateUserNameById() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.updateUserNameById(1, "root");
+        sqlSession.close();
+    }
+
+    @Test
+    public void testGetUserCount() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int count = mapper.getUserCount();
+        System.out.println("count:" + count);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testGetUserByIdToMap() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = mapper.getUserByIdToMap(1);
+        System.out.println("map:" + map);
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void testGetAllUseToMap() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<Map<String, Object>> maps = mapper.getAllUserToMap();
+        System.out.println("map:" + maps);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setUsername("haha");
+        user.setPassword("123456");
+        mapper.insertUser(user);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testDeleteUserById() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.deleteUserById(4);
+        sqlSession.close();
+    }
+
 }
+
--- MyBatisDemo/mybatis-02-params/src/main/java/com/style103/mybatis/mapper/UserMapper.java
+++ MyBatisDemo/mybatis-02-params/src/main/java/com/style103/mybatis/mapper/UserMapper.java
@@ -1,6 +1,11 @@
 package com.style103.mybatis.mapper;
 
 import com.style103.mybatis.bean.User;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /*
  * created by 103style on 2024/12/16 10:59
@@ -8,5 +13,53 @@ import com.style103.mybatis.bean.User;
  */
 public interface UserMapper {
 
+    /**
+     * 通过id查询信息
+     */
     User getUserById(int id);
+
+    /**
+     * 通过username查询信息
+     */
+    User getUserByName(String username);
+
+    /**
+     * 查询全部的数据
+     */
+    List<User> getAllUser();
+
+    /**
+     * 通过id更新对应的username
+     */
+    void updateUserNameById(int id, String name);
+
+    /**
+     * @return 获取表里数据的总条数
+     */
+    int getUserCount();
+
+    /**
+     * 通过id查找数据信息 以map结构返回
+     */
+    Map<String, Object> getUserByIdToMap(@Param("id") int id);
+
+    /**
+     * 查找所有数据信息 以map结构返回
+     */
+    //        List<Map<String, Object>> getAllUserToMap();
+    @MapKey("id")
+    List<Map<String, Object>> getAllUserToMap();
+
+    /**
+     * 通过实体类参数入库
+     * insert into t_user (username, password) values (#{username}, #{password})
+     * 需要用属性对应的名字做参数
+     */
+    void insertUser(User user);
+
+    /**
+     * 通过id删除对应的数据
+     */
+    void deleteUserById(@Param("id") int id);
+
 }
