e767c56bd8e0cd00fd45f0ce0a34af0429af601c
103style <wtimexiaoke@gmail.com>
2024-12-16 23:52:19 +0800

    mybatis 自定义映射示例

--- MyBatisDemo/mybatis-03-resultmap/src/main/java/com/style103/mybatis/mapper/EmpMapper.java
+++ MyBatisDemo/mybatis-03-resultmap/src/main/java/com/style103/mybatis/mapper/EmpMapper.java
@@ -1,9 +1,15 @@
 package com.style103.mybatis.mapper;
 
+import com.style103.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * create by 103style on 2024/12/16 23:25
  * github:https://github.com/103style
  */
 public interface EmpMapper {
 
+    Emp selectEmpById(@Param("empId") int empId);
+
+    Emp selectEmpByIdWithResultMap(@Param("empId") int empId);
 }
---
+++ MyBatisDemo/mybatis-03-resultmap/src/test/java/mybatis/ResultMapTest.java
@@ -0,0 +1,30 @@
+package mybatis;
+
+import com.style103.mybatis.mapper.EmpMapper;
+import com.style103.mybatis.pojo.Emp;
+import com.style103.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * create by 103style on 2024/12/16 23:36
+ * github:https://github.com/103style
+ */
+public class ResultMapTest {
+
+    @Test
+    public void testSelectEmpById() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.selectEmpById(1);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testSelectEmpWithResultMap() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.selectEmpByIdWithResultMap(1);
+        System.out.println(emp);
+    }
+}
