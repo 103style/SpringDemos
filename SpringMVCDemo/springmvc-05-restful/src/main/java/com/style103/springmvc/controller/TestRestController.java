33a9cbe94846f499dd81a175dd6ebc7ccfcab271
103style <wtimexiaoke@gmail.com>
2025-01-05 17:37:45 +0800

    springmvc restful 基础模块配置

---
+++ SpringMVCDemo/springmvc-05-restful/src/main/java/com/style103/springmvc/controller/TestRestController.java
@@ -0,0 +1,12 @@
+package com.style103.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * create by 103style on 2025/1/5 17:36
+ * github:https://github.com/103style
+ */
+@Controller
+public class TestRestController {
+
+}
