e1d6ce0d848a6583c8a8c58f485c9b9d1eed141a
103style <wtimexiaoke@gmail.com>
2025-01-05 17:15:49 +0800

    springmvc 视图 基础模块配置

---
+++ SpringMVCDemo/springmvc-04-view/src/main/java/com/style103/springmvc/controller/TestViewController.java
@@ -0,0 +1,11 @@
+package com.style103.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * create by 103style on 2025/1/5 17:13
+ * github:https://github.com/103style
+ */
+@Controller
+public class TestViewController {
+}
