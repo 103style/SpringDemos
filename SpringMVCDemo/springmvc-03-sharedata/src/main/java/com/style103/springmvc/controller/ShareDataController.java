19baa6a862ab33f64a7b3aa3e29ae3b92704cd0e
103style <wtimexiaoke@gmail.com>
2025-01-05 16:06:02 +0800

    springmvc 共享数据基础module配置

---
+++ SpringMVCDemo/springmvc-03-sharedata/src/main/java/com/style103/springmvc/controller/ShareDataController.java
@@ -0,0 +1,13 @@
+package com.style103.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * create by 103style on 2025/1/5 15:36
+ * github:https://github.com/103style
+ */
+@Controller
+public class ShareDataController {
+
+}
