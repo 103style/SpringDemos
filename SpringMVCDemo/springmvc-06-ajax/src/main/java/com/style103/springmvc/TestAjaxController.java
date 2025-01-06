c3d51e1ef1cfe05d4155d5cbc4d435508b7b5d4e
103style <wtimexiaoke@gmail.com>
2025-01-06 22:54:36 +0800

    springmvc ajax请求 添加@RequestBody注解获取请求内容

--- SpringMVCDemo/springmvc-06-ajax/src/main/java/com/style103/springmvc/TestAjaxController.java
+++ SpringMVCDemo/springmvc-06-ajax/src/main/java/com/style103/springmvc/TestAjaxController.java
@@ -3,6 +3,7 @@ package com.style103.springmvc;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
@@ -14,9 +15,14 @@ import java.io.IOException;
 @Controller
 public class TestAjaxController {
 
+    /**
+     * @RequestParam 获取请求参数
+     * @RequestBody 获取请求内容
+     */
     @PostMapping("/test/ajax")
-    public void testAjax(@RequestParam("id") Integer id, HttpServletResponse response) throws IOException {
+    public void testAjax(@RequestParam("id") Integer id, @RequestBody String requestBody, HttpServletResponse response) throws IOException {
         System.out.println("---------testAjax id:" + id);
+        System.out.println("---------testAjax requestBody:" + requestBody);
         response.setContentType("text/plain"); // 设置响应内容类型
         response.getWriter().write("hello, ajax.");
     }
