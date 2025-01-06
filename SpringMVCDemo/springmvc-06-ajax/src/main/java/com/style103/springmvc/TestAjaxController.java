d939330d975122cbc6d4288ef4121c1fdf96df77
103style <wtimexiaoke@gmail.com>
2025-01-06 22:38:17 +0800

    springmvc ajax请求 参数需要添加类似@RequestParam("id")的注解

--- SpringMVCDemo/springmvc-06-ajax/src/main/java/com/style103/springmvc/TestAjaxController.java
+++ SpringMVCDemo/springmvc-06-ajax/src/main/java/com/style103/springmvc/TestAjaxController.java
@@ -2,7 +2,6 @@ package com.style103.springmvc;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,12 +12,11 @@ import java.io.IOException;
  * github:https://github.com/103style
  */
 @Controller
-@CrossOrigin(origins = "http://localhost:8081") // 允许来自前端的请求
 public class TestAjaxController {
 
     @PostMapping("/test/ajax")
-    public void testAjax(@RequestParam Integer id, HttpServletResponse response) throws IOException {
-        System.out.println("id:" + id);
+    public void testAjax(@RequestParam("id") Integer id, HttpServletResponse response) throws IOException {
+        System.out.println("---------testAjax id:" + id);
         response.setContentType("text/plain"); // 设置响应内容类型
         response.getWriter().write("hello, ajax.");
     }
