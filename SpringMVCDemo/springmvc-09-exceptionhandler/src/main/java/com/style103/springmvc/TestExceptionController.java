a34484de665c0ce56c165bec4bec8889c6938b9e
103style <wtimexiaoke@gmail.com>
2025-01-11 23:48:01 +0800

    springmvc 通过注解@ControllerAdvice 和 @ExceptionHandler 处理请求异常示例

--- SpringMVCDemo/springmvc-09-exceptionhandler/src/main/java/com/style103/springmvc/TestExceptionController.java
+++ SpringMVCDemo/springmvc-09-exceptionhandler/src/main/java/com/style103/springmvc/TestExceptionController.java
@@ -1,6 +1,9 @@
 package com.style103.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,11 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * github:https://github.com/103style
  */
 @Controller
+@ControllerAdvice //将当前的类表示为异常处理的组件
 public class TestExceptionController {
 
     @RequestMapping("/test/exception")
     public String testException() {
         int a = 1 / 0;
         return "success";
+    }
+
+    // 配置要处理的异常
+    @ExceptionHandler(ArithmeticException.class)
+    public String handleException(Throwable ex, Model model) {
+        System.out.println("-------handleException ex:" + ex);
+        System.out.println("-------handleException ex:" + ex);
+        model.addAttribute("ex", ex);
+        return "error";
     }
 }
