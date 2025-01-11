4722efb54743ba28498143420e026e421e4f2797
103style <wtimexiaoke@gmail.com>
2025-01-11 23:28:05 +0800

    springmvc 异常处理 模块初始化

---
+++ SpringMVCDemo/springmvc-09-exceptionhandler/src/main/java/com/style103/springmvc/TestExceptionController.java
@@ -0,0 +1,11 @@
+package com.style103.springmvc;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * create by 103style on 2025/1/11 23:27
+ * github:https://github.com/103style
+ */
+@Controller
+public class TestExceptionController {
+}
